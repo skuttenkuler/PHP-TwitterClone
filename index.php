e7ee706663b2ca7560ef8bc1a586fd905236a049
skuttenkuler <sam.kuttenk@gmail.com>
2020-04-27 01:14:36 -0700

    form

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 
+
+print <<<EOF
+<form action=index.php method=post>
+  <table><TR><TD width=400>
+  <textarea name=tweet class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </td><TD>
+  <button type="submit" class="btn btn-primary">Tweet</button>
+  </td></tr></table>
+</form>
+EOF;
